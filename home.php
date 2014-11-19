d46504bed0d9b270ff105cf752279bc9186f0330
stankiev <stankiev@ualberta.ca>
2014-11-19 16:36:01 -0700

    Should not be able to access home page without logging in first

--- home.php
+++ home.php
@@ -1,3 +1,16 @@
+<?php
+    include_once ("helper.php"); 
+    $message = "";
+    session_start();
+    if (!$_SESSION['username']) {
+        redirect('login.php');
+        $message = "redirected to login";
+    }
+    else {
+        $message = "logged in";        
+    }
+?>
+
 <html>
 <head>
 <title>Home</title>
@@ -6,6 +19,13 @@
 <body>
 <h1><center>Home</center></h1>
 
+    <?php
+        
+        echo 'Hello ' . $_SESSION['username'];// . ' session id: ' . session_id() . ", message: " . $message;
+
+?>
+    
+</p>
 
 <form name="LogoutForm" action="login.php" method="post" >
 
--- login.php
+++ login.php
@@ -5,9 +5,9 @@ $message = "If you have registered, login using the form below";
 $php_self = $_SERVER['PHP_SELF'];
 
 if(!empty($_POST) && isset($_POST['submitLogin'])) {
+        // The user submitted information
 	$user = $_POST['userid'];
 	$pass = $_POST['password'];
-        
         
         ini_set('display_errors', 1);
         error_reporting(E_ALL);
@@ -21,6 +21,7 @@ if(!empty($_POST) && isset($_POST['submitLogin'])) {
 
         //sql command
         $sql = 'SELECT * FROM users u WHERE u.user_name = \'' . $user . '\' and u.password = \'' . $pass . '\''; 
+        
         //Prepare sql using conn and returns the statement identifier
         $stid = oci_parse($conn, $sql );
 
@@ -28,32 +29,42 @@ if(!empty($_POST) && isset($_POST['submitLogin'])) {
         $res=oci_execute($stid);
         
         if (!$res) {
+            // If there was some problem executing the statement
             $message = "Invalid username or password";
         }
         else if ($row = oci_fetch_array($stid, OCI_ASSOC)) {
+            // If there was a row matching, user supplied correct login
             $wrongLogin = false;
         }
 	
 	if ($wrongLogin) {
-		$message = "Invalid username or password";
+            // If user supplied incorrect login
+            $message = "Invalid username or password";
 	}
 	else {
-		//start session, need to give a cookie?
-		redirect('home.php');
+            session_start();
+            session_regenerate_id();
+            $_SESSION['username'] = $user;
+            
+            // Free the statement identifier when closing the connection
+            oci_free_statement($stid);
+            oci_close($conn);
+            
+            redirect('home.php');
 	}
         
         // Free the statement identifier when closing the connection
         oci_free_statement($stid);
-        oci_close($conn);
-		
+        oci_close($conn);	
 }
 
 
 if(!empty($_POST) && isset($_POST['logout'])) {
-	$message = 'Successfully logged out';
-	
-	//delete any session information? ie delete cookie??
-	
+    // Clears all session information after logout
+    session_start();
+    session_regenerate_id();
+    session_destroy();
+    $message = 'Successfully logged out, session id: ' . session_id();
 }
 
 
@@ -73,7 +84,7 @@ if(!empty($_POST) && isset($_POST['logout'])) {
 <p><?php echo $message ?></p>
 <table>
 <tr valign=top align=left>
-<td><b><i>Userid:</i></b></td>
+<td><b><i>Username:</i></b></td>
 <td><input type="text" name="userid" value="user" autofocus ><br></td>
 </tr>
 <tr valign=top align=left>
