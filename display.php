7091b171cc9f6d2ed1203f633f9b70d7afe7155c
stankiev <stankiev@ualberta.ca>
2014-11-19 22:36:46 -0700

    Users can now update some of their image information

--- display.php
+++ display.php
@@ -14,10 +14,36 @@ $place = "place";
 $description = "description";
 $owner = "";
 $deleted = 0;
+$updated = 0;
 
 if (!empty($_POST) && isset($_POST['submitEdit'])) {
     //Need to save new image values, and should probably check again that current user is owner
+
+    $place = $_POST['place'];
+    $description = $_POST['description'];
+    $subject = $_POST['subject'];
     
+    $conn=connect();
+    $photo_id = $_POST['photo_id'];
+
+    $sql = 'UPDATE images SET subject=\'' . $subject . '\', place=\'' . $place . '\', description=\'' . $description . '\' WHERE photo_id=\'' . $photo_id . '\'';
+    $stid = oci_parse($conn, $sql);
+    $row = oci_execute($stid, OCI_DEFAULT);  
+    if($row) {
+        if (oci_commit($conn)) {
+            $updated = 1;
+        }
+        else {
+            $err = oci_error($stid); 
+            echo htmlentities($err['message']);
+        }        
+    }  
+    else { 
+        $err = oci_error($stid); 
+        echo htmlentities($err['message']);
+    }
+    oci_free_statement($stid);
+    oci_close($conn);
 }
 else if (!empty($_POST) && isset($_POST['submitDelete'])) {
     //If owner clicked delete button, remove this image from database
@@ -81,13 +107,19 @@ else if (!empty($_GET) && isset($_GET['photo_id'])) {
 
 </form>
     
+<form name="SearchForm" action="search.php" method="get" >
+
+<input type="submit" value="New Search">
+
+</form>
+    
 <h1><center>Image</center></h1>
 
 
 <?php
-    //If the image exists
+    //If the image exists, show all its information
+    //TODO: add a check that the user has permission to view it
     if ($data) {
-    //And need to check that current user has permission to view this image
         echo $imageTag;
 ?>
 
@@ -99,7 +131,7 @@ else if (!empty($_GET) && isset($_GET['photo_id'])) {
 <table>
     <tr valign=top align=left>
     <td>
-        <b><i>Subject*: </i></b></td>
+        <b><i>Subject: </i></b></td>
     <td>
         <input type='text' name='subject' value="<?php echo $subject ?>" id='subject' maxlength="128" <?php if ($user != $owner && $user != 'admin') { echo 'readonly'; } ?>
         /><br>
@@ -107,14 +139,14 @@ else if (!empty($_GET) && isset($_GET['photo_id'])) {
     </tr>
     <tr valign=top align=left>
     <td>
-        <b><i>Place*: </i></b></td>
+        <b><i>Place: </i></b></td>
     <td>
         <input type='text' name='place' value="<?php echo $place ?>" id='place' maxlength="128" <?php if ($user != $owner && $user != 'admin') { echo 'readonly'; } ?>/><br>
     </td>
     </tr>
     <tr valign=top align=left>
     <td>
-        <b><i>Description*: </i></b></td>
+        <b><i>Description: </i></b></td>
     <td>
         <input type='text' name='description' value="<?php echo $description  ?>" id='description' maxlength="2048" <?php if ($user != $owner && $user != 'admin') { echo 'readonly'; } ?>/><br>
     </td>
@@ -122,7 +154,7 @@ else if (!empty($_GET) && isset($_GET['photo_id'])) {
     </table>
 
 <?php
-// If user is owner, display the Save button
+// If user is owner, display the Save and Delete buttons
 if ($user == $owner || $user == 'admin') {
 ?>
 <input type='submit' name='submitEdit' value='Save' />
@@ -136,11 +168,17 @@ if ($user == $owner || $user == 'admin') {
 
 
 <?php
-    } //End of if image exists
+    }
     else if ($deleted) {
+        // If user clicked delete button, show this message
         echo 'Image deleted';
     }
+    else if ($updated) {
+        // If user clicked save button, show this message
+        echo 'Image updated';
+    }
     else {
+        // If image could not be found, show this message
         echo 'Image not found';
     }
  ?>
