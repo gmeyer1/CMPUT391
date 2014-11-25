61329d79a44c696a3f2182a7a9759b861ef5b4ea
gmeyer1 <gmeyer1@ualberta.ca>
2014-11-25 14:05:02 -0700

    keyword searching added

--- search.php
+++ search.php
@@ -18,12 +18,29 @@ if(!empty($_POST) && isset($_POST['submitSearch'])) {
     //$message = "keywords: " . $keywords . ", after: " . $after . ", before: " . $before;
     //Search for images based on submitted conditions
     
+    $conn=connect();
+    
+    if (isset($_POST['submitSearch'])) {
+        $key_array = explode(' ', $keywords);
+        
+        $sql = 'SELECT r.photo_id, i.thumbnail, r.tot_score from ( SELECT photo_id, timing, SUM(score) as tot_score from ( SELECT photo_id, timing, ((SCORE(1) * 6) + (SCORE(2) * 3) + SCORE(3)) score FROM images WHERE CONTAINS (subject, \''.$key_array[0].'\', 1) > 0 OR CONTAINS (place, \''.$key_array[0].'\', 2) > 0 OR CONTAINS (description, \''.$key_array[0].'\', 3) > 0';
+        
+        foreach ($key_array as $key) {
+            if ($key_array[0] != $key) {
+                $sql = $sql . ' UNION ALL SELECT photo_id, timing, ((SCORE(1) * 6) + (SCORE(2) * 3) + SCORE(3)) score FROM images WHERE CONTAINS (subject, \''.$key.'\', 1) > 0 OR CONTAINS (place, \''.$key.'\', 2) > 0 OR CONTAINS (description, \''.$key.'\', 3) > 0';
+            }
+        }
+        
+        $sql = $sql . ') GROUP BY photo_id, timing ) r JOIN images i ON i.photo_id = r.photo_id ORDER BY r.tot_score DESC';
+        
+        $stid = oci_parse($conn, $sql);
+        oci_execute($stid);
+    }
     
     $images = '<br><tr><td>Search Results: </td></tr>';
     while($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
         $id = $row['PHOTO_ID'];
         $data = $row['THUMBNAIL']->load();
-        $message = "in loop";
         $images .= '<tr><td><a href=display.php?photo_id=' . $id . '><img src="data:image/jpeg;base64,'.base64_encode( $data ).'"/></a></td></tr>';            
     }
 }
@@ -60,7 +77,7 @@ else {
     <?php
         
         echo 'Hello ' . $_SESSION['username'];// . ', message: ' . $message;// . ' session id: ' . session_id() . ", message: " . $message;
-
+        
     ?>
     
 </p>
@@ -72,7 +89,6 @@ else {
 <tr valign=top align=left>
 <td><b><i>Search method:</i></b></td>
 <td>
-<input type="radio" name="searchType" value="keywords" checked="checked">Keywords<br>
 <input type="radio" name="searchType" value="newest">Newest<br>
 <input type="radio" name="searchType" value="oldest">Oldest<br>
 </td>
