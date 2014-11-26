00fc923a49890114d1ad45bd661dd0d3ebe56c0f
gmeyer1 <gmeyer1@ualberta.ca>
2014-11-25 23:53:55 -0700

    added permission checking for popularity

--- popular_view.sql
+++ popular_view.sql
@@ -12,5 +12,13 @@ WHERE photo_id not in
 
 SELECT i.photo_id, i.thumbnail FROM images i
 JOIN total_views v ON v.photo_id = i.photo_id
-WHERE v.total IN (SELECT total FROM total_views WHERE ROWNUM < 6)
+WHERE v.total IN (
+    SELECT t.total FROM total_views t
+    join images p on p.photo_id = t.photo_id 
+    WHERE ROWNUM < 6
+    and (p.owner_name = 'gmeyer1' or 'gmeyer1'='admin' or p.permitted = 1 or
+    p.permitted in (SELECT group_id FROM group_lists WHERE friend_id = 'gmeyer1'))
+)
+and (i.owner_name = 'gmeyer1' or 'gmeyer1'='admin' or i.permitted = 1 or
+i.permitted in (SELECT group_id FROM group_lists WHERE friend_id = 'gmeyer1'))
 ORDER BY v.total desc;
