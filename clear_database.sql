6fb88a2c99c417168a9441f10d5ccdbffb334ca0
gmeyer1 <gmeyer1@ualberta.ca>
2014-11-26 12:08:53 -0700

    added setup_2014f.sql

---
+++ clear_database.sql
@@ -0,0 +1,12 @@
+DROP VIEW total_views;
+DROP TABLE popular_images;
+
+DROP INDEX descIndex;
+DROP INDEX subjIndex;
+DROP INDEX placeIndex;
+
+DROP TABLE persons;
+DROP TABLE group_lists;
+DROP TABLE images;
+DROP TABLE groups;
+DROP TABLE users;
